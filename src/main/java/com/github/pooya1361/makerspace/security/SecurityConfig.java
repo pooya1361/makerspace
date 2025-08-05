65f8411b779253ebe495306904d1aa0a90b6b516
Pouya <pooya1361@gmail.com>
2025-08-06 00:04:21 +0200

    CORS fix

--- src/main/java/com/github/pooya1361/makerspace/security/SecurityConfig.java
+++ src/main/java/com/github/pooya1361/makerspace/security/SecurityConfig.java
@@ -120,7 +120,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://master.d31o1td403e37h.amplifyapp.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
