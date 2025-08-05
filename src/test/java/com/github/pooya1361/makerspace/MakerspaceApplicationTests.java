57ef23cc9ba80dca6901dc0682e3c923485292fc
Pouya <pooya1361@gmail.com>
2025-08-05 23:06:51 +0200

    Exclude test

--- src/test/java/com/github/pooya1361/makerspace/MakerspaceApplicationTests.java
+++ src/test/java/com/github/pooya1361/makerspace/MakerspaceApplicationTests.java
@@ -3,11 +3,7 @@ package com.github.pooya1361.makerspace;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MakerspaceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+class MakerspaceApplicationTests {
 
 }
